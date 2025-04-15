46e1d1d154984d5de968fd76c4b282a92bffbc8d
HuuHau235 <hhai98940@gmail.com>
2025-04-15 11:05:43 +0700

    update

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from '../Header/Header';
+import Content from '../Content/Content';
+import Footer from '../Footer/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+      </div>
+  );
+}
+
+export default App;
